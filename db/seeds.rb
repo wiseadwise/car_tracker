51d4ed80b8b69e590a68372cd0a68c708a2c5e13
Wise Advise <wiseadvise.development@gmail.com>
2011-10-07 20:38:02 +0300

    db seed upgrade

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,20 @@ Driver.create(
   :second_name => 'Sergeevich',
   :last_name => 'Driverov'
 )
+Driver.create(
+  :first_name => "Helper",
+  :second_name => 'Goodboy',
+  :last_name => 'Machanizovich'
+)
 Master.create(
   :first_name => "Master",
   :second_name => 'Sergeevich',
   :last_name => 'Masterov'
+)
+Master.create(
+  :first_name => "LoMaster",
+  :second_name => 'Petrovich',
+  :last_name => 'Lomasterov'
 )
 Vehicle.create(
   :name => 'Belaz'
@@ -25,3 +35,7 @@ Vehicle.create(
 Vehicle.create(
   :name => 'Kraz'
 )
+vehicles = Vehicle.all
+100.times do |i|
+  Trip.create(:vehicle => vehicles.sample, :created_at => Time.now - (60 * i).minutes) 
+end
